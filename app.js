0852eac1f47170ad711e04cee64e60ed3368b98d
doduyphatgmo <phat.do@kyanon.digital>
2020-03-01 16:01:20 +0700

    completed working with express - video 21

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In the Middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
-});
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res) => {
-    res.send('<h1>Hello ExpressJS</h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
